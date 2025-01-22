fade87dde9c4be8c663c0c1da4b322be3952f63d
Komal Maksane <komal20@gmail.com>
2025-01-22 17:55:29 +0530

    modified

---
+++ js/LeapYear.js
@@ -0,0 +1,15 @@
+// program to check leap year
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// Example usage
+let year = 2020;
+if (isLeapYear(year)) {
+    console.log(`${year} is a leap year.`);
+} else {
+    console.log(`${year} is not a leap year.`);
+}
---
+++ js/largest.js
@@ -0,0 +1,14 @@
+
+//take i/p as 3 number 
+
+let num1 = 8;
+let num2 = 23;
+let num3 = 17;
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log(num1 + ' is the largest number.');
+} else if (num2 >= num1 && num2 >= num3) {
+    console.log(num2 + ' is the largest number.');
+} else {
+    console.log(num3 + ' is the largest number.');
+}
